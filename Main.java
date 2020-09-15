b2fb32d21d24204c66238b10476ae335b6e2946d
blisn8405 <blisn8405@wrdsb.ca>
2020-09-15 13:57:13 +0000

    finished first question

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
  *
- * @author 
+ * @Nick bliss 
  */
 public class Main {
 
@@ -9,7 +10,30 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // create a scanner for user input
+Scanner input = new Scanner(System.in);
+
+//define if 5 is odd or even
+
+final int odd = 1;
+
+//define if 12632 is odd or even
+final int even = 0;
+
+if(5%2 >0){
+//tell them their number is odd
+ System.out.println("Your number is odd");
+} else {
+//tell them there number is even
+System.out.println("Your number is even");
+}
+if(12632%2 >0){
+//tell them their number is odd
+ System.out.println("Your number is odd");
+} else {
+//tell them there number is even
+System.out.println("Your number is even");
+  }
   }
 }
+
